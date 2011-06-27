c3e098673f917f0ae07bd843a68faf06b844a98d
Allan Caffee <allanc@aweber.com>
2011-06-27 11:27:09 -0400

    Readme and setup

---
+++ README.rst
@@ -0,0 +1,6 @@
+Deterministic Dingus
+====================
+
+This is a very simple wrapper around the Dingus library to simplify
+checking that the result of the tested is the result of calling the
+function with particular arguments.
---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+setup(name='deterministic-dingus',
+      version='0.0.1',
+      description='A handy extension',
+      long_description=file('README.rst').read(),
+      author='Allan Caffee',
+      author_email='allan.caffee@gmail.com',
+      install_requires=['dingus'],
+      py_modules=['deterministic_dingus'],
+      data_files=[('', ['README.rst'])],
+      license='BSD',
+      url='https://github.com/allancaffe/deterministic-dingus',
+      keywords='testing test mocking mock double stub fake record assert',
+      classifiers=["Development Status :: 2 - Pre-Alpha",
+                   "Intended Audience :: Developers",
+                   "License :: OSI Approved :: MIT License",
+                   "Operating System :: OS Independent",
+                   "Programming Language :: Python",
+                   "Topic :: Software Development :: Testing",
+                   ],
+     )
